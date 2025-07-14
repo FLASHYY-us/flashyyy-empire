14f40dd13ba87fd44b9344a78a1375882707dc2d
FLASHYY-us <anmolmaahi786@gmail.com>
2025-07-14 17:14:42 +0530

    Create matrix-mode.sh

---
+++ matrix-mode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo -e "\e[1;32mWelcome to MATRIX MODE - Flashyyy Empire \e[0m"
+while :
+do
+  echo "$(($RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))" | lolcat
+  sleep 0.1
+done

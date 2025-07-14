93ddd48005f02cc3cc1b210aa245e92dfcec908c
FLASHYY-us <anmolmaahi786@gmail.com>
2025-07-14 17:01:34 +0530

    Create toolx-installer.sh

---
+++ tools/toolx-installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "🚀 Installing Tool-X (20+ tools in 1)"
+pkg install git -y
+git clone https://github.com/rajkumardusad/Tool-X
+cd Tool-X
+chmod +x install.aex
+./install.aex
